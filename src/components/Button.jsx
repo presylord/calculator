7030c29f3019157aece1d2c30dcce37c86a6fc97
Presy Lord Kidlat <kidlat123@gmail.com>
2023-02-24 23:41:23 +0800

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,10 @@
 import { useGlobalContext } from "./context"
 
+
+
 const Button = ({ digit }) => {
     const { handleDigit, handleOperation, currentSession } = useGlobalContext()
+
     return (
         <button className={digit == "=" ? "equals" : ""} onClick={["+", "-", "×", "÷", "=", "c"].includes(digit) ? () => handleOperation(digit) : () => handleDigit(digit)}>{digit}</button>
     )
